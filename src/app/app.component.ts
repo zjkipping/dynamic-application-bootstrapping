f83f30e897a2fbc7ffaf4f91cfda01acd57b4def
zjkipping <zjkipping@hotmail.com>
2020-11-23 15:10:21 -0600

    File Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  template: `
+    <h1>Main Application</h1>
+  `
 })
 export class AppComponent {}
